ed94d984adfab4fb13d3e80fb83591ac360d1f60
ankurdotio <prajapatiankur8349@gmail.com>
2026-01-27 12:56:11 +0530

    Add nodemailer for email notifications and implement registration email service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
 
 /**
 * - user register controller
@@ -35,6 +36,8 @@ async function userRegisterController(req, res) {
         },
         token
     })
+
+    await emailService.sendRegistrationEmail(user.email, user.name)
 }
 
 /**
