b6bbfc5d54772103a8a2ccadf956fb27bcb3fa2b
ankurdotio <prajapatiankur8349@gmail.com>
2026-01-30 17:01:58 +0530

    Add health check route to confirm service status

--- src/app.js
+++ src/app.js
@@ -19,6 +19,11 @@ const transactionRoutes = require("./routes/transaction.routes")
 /**
  * - Use Routes
  */
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRoutes)
